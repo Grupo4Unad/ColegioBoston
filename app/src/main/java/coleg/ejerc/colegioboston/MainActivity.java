ec8ef899dc9773ff738012c2121d0090dc81b599
Pedro_Roncancio <pedromateus8@gmail.com>
2021-02-14 14:41:37 -0500

    Aplicación con las siguientes funcionalidades. 1.Creación de iniciar sesión. 2.Creación de abrir cuenta en firibase. 3.autentificacion con correo y contraseña. 4.creacion sesión recuperar Password. 5.Creacion de base de datos para la aplicación. Pedro Roncancio.

--- app/src/main/java/coleg/ejerc/colegioboston/MainActivity.java
+++ app/src/main/java/coleg/ejerc/colegioboston/MainActivity.java
@@ -1,13 +1,13 @@
 package coleg.ejerc.colegioboston;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_login,btn_registrar,btn_recuperar;
